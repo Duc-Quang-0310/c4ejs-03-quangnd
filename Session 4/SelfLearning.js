440d6eaca2444ec36ddc9e09044ec7a5ff1ca0e5
ducquang03102000 <ducquang03102000@gmail.com>
2020-05-03 10:30:18 +0700

    homework again

--- Session 4/SelfLearning.js
+++ Session 4/SelfLearning.js
@@ -10,7 +10,31 @@
     //alert("Good Byeeeeee ");
 //}
 
-let type = Number(prompt("Ban thich xe so may(co 6 xe thoi) "));
-let car = ["Honda","Suzuki","Yamaha","Kawasaki","Ducati","Triumph"];
-let carSelect = car[type];
-console.log(carSelect);
+//let type = Number(prompt("Ban thich xe so may(co 6 xe thoi) "));
+//let car = ["Honda","Suzuki","Yamaha","Kawasaki","Ducati","Triumph"];
+//let carSelect = car[type];
+//console.log(carSelect);
+
+//let car = ["Audi","Bently","BMW","Bugatti","Ferrari","Ford"];
+//for(let i = 0; i<car.length ;i++){
+   // console.log(car[i]);
+//}
+
+//let footballPlayer = ["Ronaldo","Messi","Neymar"];
+//for(let i = 0; i<footballPlayer.length;i++ ){
+   // console.log(footballPlayer[i]);
+//}
+//let typeAFootballPlayerName = prompt("Ban co muon nhap vao ten cau thu bong da nao khong (Yes-No)???");
+//if(typeAFootballPlayerName=="Yes"){
+   // let newFootballPlayer = prompt("Vi tri ma ban muon thay doi cau thu trong mang ?( 0 la rol,1 laf messi, 2 la neymar)????")
+   // let thePlayer = prompt("Nhap vao ten cau thu moi ");
+   // footballPlayer[newFootballPlayer] = thePlayer ;
+   // for(let i = 0; i<footballPlayer.length;i++ ){
+       // console.log(footballPlayer[i]);  
+    //}
+//}
+//else
+//{
+   // alert("Paii Paiiiii")
+//}
+
