0a362e7bb567fc7ca6df2a1ac15f3055bb1ccf6e
ducquang03102000 <ducquang03102000@gmail.com>
2020-05-03 10:01:28 +0700

    HomeWork

--- Session 4/SelfLearning.js
+++ Session 4/SelfLearning.js
@@ -1,11 +1,16 @@
-let footballPlayer = ["Ronaldo","Messi","Neymar"];
-console.log(footballPlayer);
-let n = prompt("Do you want to insert a football-player(Yes-No) ???");
-if ( n == "Yes"){
-    let player = prompt("The name of your football-player");
-    footballPlayer.push(player);
-    console.log(footballPlayer);
-}
-else{
-    alert("Good Byeeeeee ");
-}
+//let footballPlayer = ["Ronaldo","Messi","Neymar"];
+//console.log(footballPlayer);
+//let n = prompt("Do you want to insert a football-player(Yes-No) ???");
+//if ( n == "Yes"){
+    //let player = prompt("The name of your football-player");
+    //footballPlayer.push(player);
+    //console.log(footballPlayer);
+//}
+//else{
+    //alert("Good Byeeeeee ");
+//}
+
+let type = Number(prompt("Ban thich xe so may(co 6 xe thoi) "));
+let car = ["Honda","Suzuki","Yamaha","Kawasaki","Ducati","Triumph"];
+let carSelect = car[type];
+console.log(carSelect);
